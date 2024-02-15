a168f84e0dda8ebb623e5104f74ed50b6a7422b4
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-16 02:41:08 +0800

    Implemented Trade

---
+++ Libs/RichillCapital.Domain/Trading/Side.cs
@@ -0,0 +1,15 @@
+using RichillCapital.SharedKernel;
+
+namespace RichillCapital.Domain.Trading;
+
+public sealed record class Side : Enumeration<Side>
+{
+    public static readonly Side Long = new(nameof(Long), 1);
+
+    public static readonly Side Short = new(nameof(Short), -1);
+
+    private Side(string name, int value)
+        : base(name, value)
+    {
+    }
+}
--- Libs/RichillCapital.Domain/Trading/Trade.cs
+++ Libs/RichillCapital.Domain/Trading/Trade.cs
@@ -1,20 +1,115 @@
 using RichillCapital.SharedKernel;
+using RichillCapital.SharedKernel.Monad;
 
 namespace RichillCapital.Domain.Trading;
 
 public sealed class Trade : ValueObject
 {
-    private Trade()
+    private Trade(
+        Side side,
+        Symbol symbol,
+        decimal quantity,
+        DateTimeOffset entryTime,
+        decimal entryPrice,
+        DateTimeOffset exitTime,
+        decimal exitPrice,
+        decimal commission,
+        decimal tax,
+        decimal swap)
     {
+        Side = side;
+        Symbol = symbol;
+        Quantity = quantity;
+        EntryTime = entryTime;
+        EntryPrice = entryPrice;
+        ExitTime = exitTime;
+        ExitPrice = exitPrice;
+        Commission = commission;
+        Tax = tax;
+        Swap = swap;
     }
 
-    public static Trade Create()
+    public Side Side { get; private set; }
+
+    public Symbol Symbol { get; private set; }
+
+    public decimal Quantity { get; private set; }
+
+    public DateTimeOffset EntryTime { get; private set; }
+
+    public decimal EntryPrice { get; private set; }
+
+    public DateTimeOffset ExitTime { get; private set; }
+
+    public decimal ExitPrice { get; private set; }
+
+    public decimal Commission { get; private set; }
+
+    public decimal Tax { get; private set; }
+
+    public decimal Swap { get; private set; }
+
+    public static ErrorOr<Trade> Create(
+        Side side,
+        Symbol symbol,
+        decimal quantity,
+        DateTimeOffset entryTime,
+        decimal entryPrice,
+        DateTimeOffset exitTime,
+        decimal exitPrice,
+        decimal commission,
+        decimal tax,
+        decimal swap)
     {
-        return new Trade();
+        if (quantity <= 0)
+        {
+            return Error.Invalid("Quantity must be greater than 0.");
+        }
+
+        if (entryPrice <= 0)
+        {
+            return Error.Invalid("Entry price must be greater than 0.");
+        }
+
+        if (exitPrice <= 0)
+        {
+            return Error.Invalid("Exit price must be greater than 0.");
+        }
+
+        if (commission < 0)
+        {
+            return Error.Invalid("Commission cannot be negative.");
+        }
+
+        if (tax < 0)
+        {
+            return Error.Invalid("Tax cannot be negative.");
+        }
+
+        return new Trade(
+            side,
+            symbol,
+            quantity,
+            entryTime,
+            entryPrice,
+            exitTime,
+            exitPrice,
+            commission,
+            tax,
+            swap);
     }
 
     protected override IEnumerable<object> GetAtomicValues()
     {
-        throw new NotImplementedException();
+        yield return Side;
+        yield return Symbol;
+        yield return Quantity;
+        yield return EntryTime;
+        yield return EntryPrice;
+        yield return ExitTime;
+        yield return ExitPrice;
+        yield return Commission;
+        yield return Tax;
+        yield return Swap;
     }
 }
