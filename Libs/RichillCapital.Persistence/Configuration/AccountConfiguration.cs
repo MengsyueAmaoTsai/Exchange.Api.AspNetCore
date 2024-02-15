e40d9db471b92e75fa6f20962ee8e81346e347d2
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-16 03:31:33 +0800

    add account configuration

---
+++ Libs/RichillCapital.Persistence/Configuration/AccountConfiguration.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using RichillCapital.Domain.Trading;
+using RichillCapital.SharedKernel.EntityFrameworkCore;
+
+namespace RichillCapital.Persistence.Configurations;
+
+internal sealed class AccountConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder
+            .ToTable("accounts")
+            .HasKey(account => account.Id);
+
+        builder
+            .Property(account => account.Id)
+            .HasColumnName("id")
+            .HasMaxLength(AccountId.MaxLength)
+            .HasConversion(
+                id => id.Value,
+                value => AccountId.From(value).Value)
+            .IsRequired();
+
+        builder
+            .Property(account => account.Name)
+            .HasColumnName("name")
+            .HasMaxLength(AccountName.MaxLength)
+            .HasConversion(
+                name => name.Value,
+                value => AccountName.From(value).Value)
+            .IsRequired();
+
+        builder
+            .Property(account => account.Currency)
+            .HasColumnName("currency")
+            .HasEnumerationValueConversion()
+            .IsRequired();
+    }
+}
