9b095206a92a29a2a207c97c1d0dde8f04a4a859
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-15 23:53:30 +0800

    Add extensions

---
+++ Libs/RichillCapital.SharedKernel.EntityFrameworkCore/EnumerationTypePropertyBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RichillCapital.SharedKernel.EntityFrameworkCore;
+
+public static class EnumerationTypePropertyBuilderExtensions
+{
+    public static PropertyBuilder<TProperty> HasEnumerationValueConversion<TProperty>(
+        this PropertyBuilder<TProperty> builder)
+        where TProperty : Enumeration<TProperty> =>
+        builder.HasConversion(
+            enumeration => enumeration.Value,
+            value => Enumeration<TProperty>.FromValue(value).Value);
+
+    public static PropertyBuilder<TProperty> HasEnumerationNameConversion<TProperty>(
+        this PropertyBuilder<TProperty> builder, bool ignoreCase = false)
+        where TProperty : Enumeration<TProperty> =>
+        builder
+            .HasMaxLength(Enumeration<TProperty>.Members
+                .Max(member => member.Name.Length))
+            .HasConversion(
+                enumeration => enumeration.Name,
+                name => Enumeration<TProperty>.FromName(name, ignoreCase).Value);
+}
