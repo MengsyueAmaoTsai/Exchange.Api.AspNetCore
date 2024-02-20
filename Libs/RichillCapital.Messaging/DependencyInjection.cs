592d824d3734c3aa650fb3b45edb4ee0edc7ded3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-21 05:45:54 +0800

    add Messaging options

--- Libs/RichillCapital.Messaging/DependencyInjection.cs
+++ Libs/RichillCapital.Messaging/DependencyInjection.cs
@@ -6,6 +6,20 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddMessaging(this IServiceCollection services)
     {
+        services.AddPersistenceOptions();
+
+        return services;
+    }
+
+
+    private static IServiceCollection AddPersistenceOptions(this IServiceCollection services)
+    {
+        services
+            .AddOptions<MessagingOptions>()
+            .BindConfiguration(nameof(MessagingOptions))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
         return services;
     }
 }
---
+++ Libs/RichillCapital.Messaging/MessagingOptions.cs
@@ -0,0 +1,5 @@
+namespace RichillCapital.Messaging;
+
+public sealed record class MessagingOptions
+{
+}
