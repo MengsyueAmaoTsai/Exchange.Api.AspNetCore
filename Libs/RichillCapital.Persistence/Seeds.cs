d1a38ab4a790baa215d7d74abcaf79a68d184376
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-03-05 11:02:47 +0800

    add seed account

--- Libs/RichillCapital.Persistence/Seeds.cs
+++ Libs/RichillCapital.Persistence/Seeds.cs
@@ -2,13 +2,18 @@ using System.Collections.ObjectModel;
 
 using Microsoft.Extensions.DependencyInjection;
 
+using RichillCapital.Domain.Shared;
 using RichillCapital.Domain.Trading;
 
 namespace RichillCapital.Persistence;
 
 public static class Seeds
 {
-    private static readonly Collection<Account> Accounts = [];
+    private static readonly Collection<Account> Accounts = [
+        Account.Create(AccountId.From("1").Value, AccountName.From("test1").Value, PositionMode.Hedging, TradingEnvironment.Mock, Currency.TWD).Value,
+        Account.Create(AccountId.From("1").Value, AccountName.From("test2").Value, PositionMode.Hedging, TradingEnvironment.Simulated, Currency.TWD).Value,
+    ];
+
     private static readonly Collection<Order> Orders = [];
     private static readonly Collection<Execution> Executions = [];
     private static readonly Collection<Trade> Trades = [];
@@ -16,6 +21,8 @@ public static class Seeds
     public static void Initialize(IServiceProvider serviceProvider)
     {
         using var context = serviceProvider.GetRequiredService<PostgreSqlDbContext>();
+
+        context.Set<Account>().AddRange(Accounts);
 
         context.SaveChanges();
     }
